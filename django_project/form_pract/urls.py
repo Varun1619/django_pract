9c61a3e7977141f2d516584f58e514bbbb953bb0
Varun1619 <varun1619singh@gmail.com>
2022-06-09 17:34:40 +0530

    path

--- django_project/form_pract/urls.py
+++ django_project/form_pract/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-path('', views.index, name  = "index"),
+    path('', views.index, name  = "index"),
     path('read/', views.read, name = 'read'),
     path('update/<str:pk>/', views.update, name = 'update'),
     path('delete/<str:pk>/', views.delete_data, name = 'delete')
